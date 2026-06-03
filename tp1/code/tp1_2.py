927f9260bcaba5280e7b17af460bce14166746b0
Ignagx <grmngl90@gmail.com>
2026-06-03 19:07:38 -0300

    Corrige valor ideal del inductor

--- tp1/code/tp1_2.py
+++ tp1/code/tp1_2.py
@@ -14,7 +14,7 @@ Yr1, Yc1, Yl1, s = sp.symbols('Yr1 Yc1 Yl1 s')
 A = ((Yc1*s)+(Yl1/s))/(100000*(Yc1*Yl1+Yc1*Yr1*s+Yl1*Yr1/s))
 H = sp.simplify(A)
 TF= TransferFunction(H.as_numer_denom()[0], H.as_numer_denom()[1], s)
-TF1=TF.xreplace({Yc1: 0.00000318, Yr1: 0.00001, Yl1: 0.03142})
+TF1=TF.xreplace({Yc1: 0.00000318, Yr1: 0.00001, Yl1: 31.42})
 pole_zero_plot(TF1)
 bode_plot(TF1,initial_exp=np.log10(0.0628), final_exp=np.log10(62831853.07))
 numerador = TF1.num
